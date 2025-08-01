10b30d6526d8553ea6db9685e1d329cdf993724a
gxnny19 <wkdrudrjs010@gmail.com>
2025-08-01 16:34:07 +0900

    js

---
+++ js2507/day06_index_545.js
@@ -0,0 +1,12 @@
+for (let i = 2; i <= 9; i++) {
+    document.writeln(`<div class= "times">`);
+    document.writeln(`<h3>${i}단</h3>`);
+    for (let j = 1; j <= 9; j++) {
+        document.writeln(`${i} X ${j} = ${i * j} <br>`);
+    }
+    document.writeln(`</div>`);
+
+    if (i === 4) {
+        break;
+    }
+}
